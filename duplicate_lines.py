8263a60f828c20a5d43c08f257b817152226165f
Tibor Kotosz <tkotosz@inviqa.com>
2020-04-24 19:00:42 +0200

    Fix caret position when duplicating a single line upwards while at the end of the line

--- duplicate_lines.py
+++ duplicate_lines.py
@@ -2,6 +2,7 @@ import sublime, sublime_plugin
 
 class DuplicateLinesCommand(sublime_plugin.TextCommand):
     def run(self, edit, up = False):
+        last_caret_region = [(selection.begin(), selection.end()) for selection in self.view.sel()]
         for region in self.view.sel():
             line = self.view.line(region)
             line_contents = self.view.substr(line)
@@ -10,3 +11,8 @@ class DuplicateLinesCommand(sublime_plugin.TextCommand):
               self.view.insert(edit, line.end(), "\n" + line_contents)
             else:
               self.view.insert(edit, line.begin(), line_contents + "\n")
+
+        if up:
+            self.view.sel().clear()
+            for selection in last_caret_region:
+                self.view.sel().add(sublime.Region(selection[0], selection[1]))
